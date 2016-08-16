d09ba1b5a449c0948250a51c38e6c0f88afdb7ba
rwbaer <rwbaer@hotmail.com>
2016-08-16 11:54:03 -0500

    fix exuberance

--- makehuman/shared/bvh.py
+++ makehuman/shared/bvh.py
@@ -251,7 +251,7 @@ class BVH():
         return name in self.joints
 
     def __cacheGetJoints(self):
-        from Queue import deque
+        from queue import deque
 
         result = []
         queue = deque([self.rootJoint])
